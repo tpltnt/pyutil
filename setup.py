2a443eddfed9a4ad949ea9387da8c7378311dbad
tpltnt <1172976+tpltnt@users.noreply.github.com>
2026-05-31 16:53:23 +0200

    style fixes setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8-with-signature-unix; fill-column: 77 -*-
 # -*- indent-tabs-mode: nil -*-
+"""
+pyutil -- utility functions and classes
 
-# pyutil -- utility functions and classes
-#
-# This file is part of pyutil; see README.rst for licensing terms.
-
-import versioneer
+This file is part of pyutil; see README.rst for licensing terms.
+"""
 from setuptools import setup
+import versioneer
 
 setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
 )
-
